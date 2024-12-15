01fc444e4f5c9322c814f51066e978befbd9ec1a
Mehmood-Wattoo <mehmoodwattoo712@gmail.com>
2024-12-15 12:21:55 +0000

    container config2

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer JS version
+
+//alert(3+3) // we are using nodeJS, not browser
+
+// code readibilty should be high
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => still value should be assigned
+// symbol => unique
+
+// object
+let name = "mehmood"
+let age = 24
+
+console.table([typeof name, typeof age])
