3ffa94dbb8d74ab64b0c753d053cc713b0412d0c
Mehmood-Wattoo <mehmoodwattoo712@gmail.com>
2024-12-15 12:00:06 +0000

    container config done1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "mehmood@gmial.com"
+var accountPassword = "12345"
+accountCity = "khushab"
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity])
+
