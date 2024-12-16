e1c9aa8aa14abf0cd34620b1b8dccd6a4a4e4a40
Mehmood-Wattoo <mehmoodwattoo712@gmail.com>
2024-12-16 21:20:27 +0000

    Stack and Heap Memory

---
+++ 01_basics/06_s_h_m.js
@@ -0,0 +1,9 @@
+// stack (primitive), Heap (non-primitive)
+
+// ***************** stack memory *****************
+//  if
+let myYTname = "mehmood@google.com"
+let otherYTname = myYTname
+otherYTname = "chaiaurcode"
+console.table([myYTname, otherYTname]);
+
