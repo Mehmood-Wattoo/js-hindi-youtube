95375756e6370112011c2cad584e52e414a8ab87
Mehmood-Wattoo <mehmoodwattoo712@gmail.com>
2024-12-16 21:32:15 +0000

    Stack and Heap Memory

--- 01_basics/06_s_h_m.js
+++ 01_basics/06_s_h_m.js
@@ -6,4 +6,15 @@ let myYTname = "mehmood@google.com"
 let otherYTname = myYTname
 otherYTname = "chaiaurcode"
 console.table([myYTname, otherYTname]);
+// +++++ explain === in stack memory when we put first datatype into second datatype, the value of first datatype is not changed because copy of first datatype is created and the value of that copy is changed
 
+let userOne = {
+    email: "user@google.com",
+}
+
+let userTwo = userOne
+
+userTwo.email = "sultan@google.com"
+
+console.table([userOne.email, userTwo.email])
+// +++++++ explain === in heap, when first datatype is put into second datatype, second datatype get reference from heap memory and value of both first and second datatypes is changed
