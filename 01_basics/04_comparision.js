7988b3358e16b44b1a740131fd252c76da97509f
Mehmood-Wattoo <mehmoodwattoo712@gmail.com>
2024-12-16 11:14:08 +0000

    update comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,1 +1,18 @@
-console.log(2 > 1);
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // here 2 will convert in number, then it will be greater than 1
+// console.log("02" > 1); // here 02 will convert in number, then it will be greater than 1
+
+console.log(null > 0); // default null is not greater than zero
+console.log(null == 0); // default null is not equal to zero
+console.log(null >= 0); // here null is conert in zero then it will be equal to zero
+
+// strict check === here datatypes and value are compared
+console.log("2" === 2); // always false
+
+// conversion from line 8 to line 16 should be always avoided to maintain clean code
