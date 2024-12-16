4e784f31ef168dc8e6df53a58dacb94b444cc4d1
Mehmood-Wattoo <mehmoodwattoo712@gmail.com>
2024-12-16 10:47:33 +0000

    again update conversion and operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,36 @@
 let score = "33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 //
-//
+// **************************Arithematic Operation *******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// ************************* common arithematic operators
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*3);
+//console.log(2**3);
+//console.log(3/2);
+//console.log(3%2);
+
+let str1 = "hello"
+let str2 = "mehmood"
+let str3 = str1 + str2 // we can only add strings not minus
+console.log(str3);
+
+console.log("1" + 2); // ans is 12
+console.log(1 + "2"); // ans is 12
+console.log("1" + 2 + 2); // answer is 122, acc to ECMA script
+console.log(1 + 2 + "2"); // answer is 32, acc to ECMA script
+
+// ************ right way to conversion
+console.log((2+2) * 3 % 5);
