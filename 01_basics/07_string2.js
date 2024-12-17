c2589ed6f141eb715c5e6d0fc111d5f98246d862
Mehmood-Wattoo <mehmoodwattoo712@gmail.com>
2024-12-17 11:24:04 +0000

    String Explore

---
+++ 01_basics/07_string2.js
@@ -0,0 +1,7 @@
+// here we practice the common strings in Javascript
+
+const gameName = new String('mehmood-mc-bd')
+// here string acts as object and give the following functions
+
+// ************** anchor() ***************
+console.table([`original is${gameName}`, `now anchor is${gameName.anchor("mc")}`])
