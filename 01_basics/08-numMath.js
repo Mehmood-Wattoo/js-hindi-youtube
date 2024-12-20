1f44bda53fcf2dd78dc873cd03cae76a29af0325
Mehmood-Wattoo <mehmoodwattoo712@gmail.com>
2024-12-20 12:21:46 +0000

    numbers and maths U3

--- 01_basics/08-numMath.js
+++ 01_basics/08-numMath.js
@@ -21,3 +21,12 @@ console.table([Math.abs(-5), Math.round(3.6)])
 console.table([Math.ceil(5.2), Math.floor(5.9)])
 console.table([`min value in the array is ${Math.min(2,4,35,6)}`, `max value in the array is ${Math.max(2,4,35,6)}` ])
 
+console.log("check");
+console.table([`square root of 25 is ${Math.sqrt(25)}`, `power of 6 is ${Math.pow(6,2)}`])
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
