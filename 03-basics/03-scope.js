59ccc34829fc2f0409cce269f712e5b2bbdb5f38
Mehmood-Wattoo <mehmoodwattoo712@gmail.com>
2024-12-24 15:26:02 +0000

    scope M

---
+++ 03-basics/03-scope.js
@@ -0,0 +1,16 @@
+// var c = 300 // if we see below 30 will overwrite this value due to its global scope
+let a = 300
+
+if (true) { // scope
+    let a = 10 // here a will not execute due to its block scope
+    console.log(`inner ${a}`);
+const b = 20 // here b will not execute due to its block scope
+// var c = 30 // here c will execute due to global scope
+}
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+// global scope in browser and global scope in node are different 
