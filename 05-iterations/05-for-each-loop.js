961e5008db79cf6116be06bfe7a0bcd5970034b1
Mehmood-Wattoo <mehmoodwattoo712@gmail.com>
2024-12-27 11:46:24 +0000

    for each loop

---
+++ 05-iterations/05-for-each-loop.js
@@ -0,0 +1,32 @@
+const coding = ['js', 'cpp', 'java']
+
+// coding.forEach( function (item) {
+//     console.log(item);
+    
+// } )
+
+coding.forEach( (item) => {
+    console.log(item);
+    
+} )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFile: "js"
+    },
+    {
+        languageName: "java",
+        languageFile: "java"
+    },
+    {
+        languageName: "python",
+        languageFile: "py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+    
+}  )
+
